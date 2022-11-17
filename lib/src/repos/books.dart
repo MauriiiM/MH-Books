4d32b8bf7465d00d86af064fa99a123de1108369
Mauricio <mauriiim@users.noreply.github.com>
2022-11-16 17:56:33 -0700

    add book search function

--- lib/src/repos/books.dart
+++ lib/src/repos/books.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 
-import 'package:mh_books/src/models/books.dart';
+import 'package:mh_books/src/models/book.dart';
 import 'package:http/http.dart' as http;
 
 class BooksRepository {
+  static const api = 'https://api.itbook.store/1.0';
+
+  /// Get new releases books.
   static Future<List<Book>> fetchNewBooks() async {
-    final response =
-        await http.get(Uri.parse('https://api.itbook.store/1.0/new'));
+    final response = await http.get(Uri.parse('$api/new'));
 
     if (response.statusCode != 200) {
       throw Exception('Failed to fetch new books');
@@ -25,9 +27,30 @@ class BooksRepository {
     return responseBooks.map((bookJson) => Book.fromJson(bookJson)).toList();
   }
 
-  // static Future> searchBooks({
-  //   required final String titleQuery,
-  // }) async {
-  //   return [];
-  // }
+  /// Search books by title, author, ISBN or keywords. Formatted as:
+  /// `/search/{query}`
+  /// `/search/{query}/{page}`
+  static Future<List<Book>> searchBooks({
+    final String? query,
+    final String? page,
+  }) async {
+    final response =
+        await http.get(Uri.parse('$api/search${page != null ? '/$page' : ''}'));
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to fetch new books');
+    }
+
+    final decodedResponse = jsonDecode(response.body);
+    if (decodedResponse is! Map<String, dynamic>) {
+      throw const FormatException();
+    }
+
+    final responseBooks = decodedResponse['books'];
+    if (responseBooks is! List) {
+      throw const FormatException();
+    }
+
+    return responseBooks.map((bookJson) => Book.fromJson(bookJson)).toList();
+  }
 }
